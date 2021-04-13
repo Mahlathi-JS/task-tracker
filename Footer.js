69d8c623e856cdbcd20167558847b422124f915d
Yamkela Mahlathi <68697639+Mahlathi-JS@users.noreply.github.com>
2021-04-13 15:31:26 +0200

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Designed by Yamkela Mahlathi &copy; 2021</p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
